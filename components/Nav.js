3f56e466f259d56c044df367b86a1c2530a511fb
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-10-05 17:09:35 -0700

    create basic home and about pages

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+  <div>
+    <h2>Masaki Kleinkopf</h2>
+    <p> I am a front-end developer with a passion for learning and creativity. I recently switched careers after years in the music industry and music academia. </p>
+  </div>
+  )
+}
+
+export default About
