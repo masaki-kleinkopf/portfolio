a64054d6c3af7d0e049526755c6959dc16c36a6c
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-11-07 16:34:49 -0800

    feat:add music links

--- pages/music.js
+++ pages/music.js
@@ -1,9 +1,10 @@
+import styles from '../styles/Music.module.scss'
+
 const Music = () => {
     return (
-      <section>
-        <p>
-          Music:
-        </p>
+      <section className={styles.container}>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Hr_oAeCzWMo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ACMOm9fnErY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </section>
       )
   }
