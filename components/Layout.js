4601bedccdde41c4e8a09c2e4b890fa3bdc29483
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-11-10 08:00:24 -0800

    add initial responsive styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
 import Nav from "./Nav"
+import styles from '../styles/layout.module.scss'
 
 export default function Layout({ children }) {
   return (
-  <>
+  <div className={styles.container}>
     <Nav />
     <main>{children}</main>
-  </>
+  </div>
   )
 }
